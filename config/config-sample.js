81fdc7ac296b0f1080fb3653c3cbdca0d80379ed
Qusai Jouda <qusai@Qusais-Device.local>
2017-03-29 12:05:00 -0400

    refactoring

--- config/config-sample.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  url: 'xxxxxx/get_data?date=',
-  kinds: ['temperature', 'humidity', 'precipitation'],
-  start_date: '09/15/2016',
-  blob: {
-    storage_account: 'xxxxx',
-    key1: 'xxxxxxxx=='
-  }
-};
